b4ff69efb103641f52f942fe8ed7ae7d74ae0e14
Vagif Gafarov <v_k_special@yahoo.com>
2024-11-17 19:10:16 +0000

    add error message if subnets are in diff AZones

--- module_nat_networking/locals.tf
+++ module_nat_networking/locals.tf
@@ -5,4 +5,7 @@ locals {
   private_subnet_az = data.aws_subnet.private.availability_zone
   # Check if both subnets share the same AZ
   subnets_are_in_same_az = local.public_subnet_az == local.private_subnet_az
+
+  # nessage if validation failed
+  error_in_validation_message = "Public subnet AZ (${local.public_subnet_az}) and Private subnet AZ (${local.private_subnet_az}) shall be in the same availability zone as per user req't"
 }
