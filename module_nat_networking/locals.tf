073b43363ea1f152bd143afadcb463f6fe067f46
Vagif Gafarov <v_k_special@yahoo.com>
2024-11-17 19:07:34 +0000

    add extra local vars for subnets az's

--- module_nat_networking/locals.tf
+++ module_nat_networking/locals.tf
@@ -1,3 +1,6 @@
 locals {
   igw_id = try(data.aws_internet_gateway.does_exists.id, aws_internet_gateway.new_igw[0].id)
+   
+  public_subnet_az  = data.aws_subnet.public.availability_zone
+  private_subnet_az = data.aws_subnet.private.availability_zone
 }
